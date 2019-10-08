5843cff517926706b5835c2b6088bed00815b163
jacksonv3058 <56265353+jacksonv3058@users.noreply.github.com>
2019-10-08 11:00:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ownerName = "Jackson Valentin"
-let userName = "YourUsername";
+let userName = "Jacksonv3058";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
